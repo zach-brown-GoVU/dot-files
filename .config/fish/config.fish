9d1f3a76b69a10f2c1e238988b3b3bce0add6ef8
Zach Brown <zacharyc.brown@veteransunited.com>
2026-02-03 08:05:18 -0600

    feat: move fish initialization into config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,22 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
+  # -- Starship --
   starship init fish | source
+  enable_transience
+
+  # -- Nix --
+  alias drs="sudo darwin-rebuild switch"
+  
+  # -- zoxide --
+  alias cd="z"
+  zoxide init fish | source
+
+  # -- eza --
+  alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+
+  # -- bat --
+  alias cat="bat"
+
+  # -- fzf --
+  fzf --fish | source
 end
