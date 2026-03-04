a470e0e76733d74cf040e1b0709f60cfcd1f56f8
Zach Brown <zacharyc.brown@veteransunited.com>
2026-03-03 21:01:06 -0600

    Revert "feat: homemanager"
    This reverts commit cb417391619410742ef01cf82966a151fc6b08a6.

---
+++ .config/fish/config.fish
@@ -0,0 +1,31 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+  # -- Starship --
+  starship init fish | source
+  enable_transience
+
+  # -- lazygit --
+  export XDG_CONFIG_HOME="$HOME/.config"
+
+  # -- Nix --
+  alias drs="sudo darwin-rebuild switch"
+
+  # -- zoxide --
+  alias cd="z"
+  zoxide init fish | source
+
+  # -- thefuck --
+  thefuck --alias | source
+
+  # -- eza --
+  alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+
+  # -- bat --
+  alias cat="bat"
+
+  # -- fzf --
+  fzf --fish | source
+
+  # -- fastfetch --
+  fastfetch
+end
--- .config/nix/modules/vu.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-
-{
-  # Packages specific to VU workstation
-  environment.systemPackages = [
-    pkgs.dotnetCorePackages.sdk_8_0_1xx-bin
-  ];
-}
